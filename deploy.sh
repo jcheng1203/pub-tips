03fa0cb9a15e9c4b366e2c9d443aac1b99dad615
Jenny <jennyjqcheng@gmail.com>
2019-04-10 00:53:14 -0700

    Bad copypasta

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,4 @@ hugo
 cd public && git add --all && git commit -m "Publishing to gh-pages (deploy.sh)"
 
 # publish
-# git push origin gh-pages"
-# rm -rf public/*
-
-echo "Generating site"
-hugo
-
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+# git push origin gh-pages
